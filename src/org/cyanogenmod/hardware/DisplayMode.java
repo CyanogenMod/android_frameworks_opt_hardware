65994c4dcb7d7025f493ccb000fbe761544efd73
Steve Kondik <steve@cyngn.com>
2015-08-15 20:57:42 -0700

    hardware: Add DisplayMode API
     * A device may have a preset list of display modes identified
       by strings. This API adds support for enumerating and
       selecting one of these modes.
    
    Change-Id: I01f4655a31d79e5c55d318afc71204b9fc5e0ec5

---
+++ src/org/cyanogenmod/hardware/DisplayMode.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.hardware;
+
+/* 
+ * Display Modes API
+ *
+ * A device may implement a list of preset display modes for different
+ * viewing intents, such as movies, photos, or extra vibrance. These
+ * modes may have multiple components such as gamma correction, white
+ * point adjustment, etc, but are activated by a single control point.
+ *
+ * This API provides support for enumerating and selecting the
+ * modes supported by the hardware.
+ *
+ * A DisplayMode is referenced by it's identifier and carries an
+ * associated name (up to the user to translate this value).
+ */
+public class DisplayMode {
+    public final int id;
+    public final String name;
+       
+    public DisplayMode(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
