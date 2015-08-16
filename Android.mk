1588fc5930289343fb271d826bc4bc98cbdebfeb
Steve Kondik <steve@cyngn.com>
2015-08-15 20:57:42 -0700

    hardware: Add DisplayModeControl API
     * A device may have a preset list of display modes identified
       by strings. This API adds support for enumerating and
       selecting one of these modes.
    
    Change-Id: I01f4655a31d79e5c55d318afc71204b9fc5e0ec5

--- Android.mk
+++ Android.mk
@@ -36,6 +36,7 @@ LOCAL_SRC_FILES += $(default_classes)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := org.cyanogenmod.hardware
+LOCAL_JAVA_LIBRARIES := org.cyanogenmod.platform.sdk
 
 include $(BUILD_JAVA_LIBRARY)
 
